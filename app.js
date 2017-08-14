c33b20b8a2ee3f21dc8291830a9f71df4ea6a00a
Eric Abell <ericabell@users.noreply.github.com>
2017-08-14 13:56:43 -0500

    Daylight savings time

--- app.js
+++ app.js
@@ -3,3 +3,9 @@ let chalk = require('chalk');
 
 console.log('It is ' + chalk.blue(moment().format("dddd, MMMM Do YYYY, h:mm:ss a")));
 console.log('It is the ' + chalk.bgMagenta(moment().format("DDDo")) + ' day of the year.');
+let now = moment();
+// let startOfDay = now.startOf('day');
+// console.log('It is ' + (now.subtract(startOfDay)) + ' seconds into the day.');
+let dst = (now.isDST()) ? 'is' : 'is not'
+console.log('It ' + dst + ' during Daylight Savings Time.');
+console.log('It ' +  + ' not a leap year.');
