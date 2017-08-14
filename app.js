ab2043c1571f5c4147c1f86b77630ed19b328838
Eric Abell <ericabell@users.noreply.github.com>
2017-08-14 14:06:29 -0500

    Done

--- app.js
+++ app.js
@@ -3,10 +3,14 @@ let chalk = require('chalk');
 
 console.log('It is ' + chalk.blue(moment().format("dddd, MMMM Do YYYY, h:mm:ss a")));
 console.log('It is the ' + chalk.bgMagenta(moment().format("DDDo")) + ' day of the year.');
+
 let now = moment();
-// let startOfDay = now.startOf('day');
-// console.log('It is ' + (now.subtract(startOfDay)) + ' seconds into the day.');
+let startOfDay = now.startOf('day'); // .startOfDay mutates now
+now = moment();  // so re-create now with the current time
+
+console.log('It is ' + chalk.cyan(now.diff(startOfDay, 'seconds')) + ' seconds into the day.');
+
 let dst = (now.isDST()) ? 'is' : 'is not'
 console.log('It ' + chalk.green(dst) + ' during Daylight Savings Time.');
 let leap = (now.isLeapYear()) ? 'is' : 'is not'
-console.log('It ' + leap + ' a leap year.');
+console.log('It ' + chalk.red(leap) + ' a leap year.');
