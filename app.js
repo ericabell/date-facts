f8c338230f0de94febe104d0dab0a60b2cb9fb5e
Eric Abell <ericabell@users.noreply.github.com>
2017-08-14 12:44:55 -0500

    Second line done

--- app.js
+++ app.js
@@ -2,3 +2,4 @@ let moment = require('moment');
 let chalk = require('chalk');
 
 console.log('It is ' + chalk.blue(moment().format("dddd, MMMM Do YYYY, h:mm:ss a")));
+console.log('It is the ' + chalk.bgMagenta(moment().format("DDDo")) + ' day of the year.');
